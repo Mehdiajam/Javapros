07703c32276b4ba8e4aabd9e97d895727dcd1d10
Mehdiajam <Mehdi.Ajam@esprit.tn>
2024-10-06 14:45:46 +0100

    first

---
+++ prosit.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class ZooManagement {
+    public static void main(String[] args) {
+        /*Scanner scanner = new Scanner(System.in);
+        //scanner.nextLine();
+        //on l'utilise wa9t me te5demch scanner ta3 string ki yebda fama 9balha NextInt()
+
+        System.out.print("Entrez le nombre de cages: ");
+        int nbrCages =0;
+        while(!scanner.hasNextInt()){
+            System.out.print("Entrez le nombre de cages: ");
+             scanner.next();
+        }
+        nbrCages = scanner.nextInt();
+
+
+        System.out.print("Entrez le nom du zoo: ");
+        String zooName = scanner.next();
+
+
+        System.out.println(zooName + " comporte " + nbrCages + " cages");*/
